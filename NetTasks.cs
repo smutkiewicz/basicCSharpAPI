a79c42e73d83d3969a9795d1f064b9fc262fe01a
smutkiewicz <smutkiewicz@gmail.com>
2016-12-31 22:49:41 +0100

    Update NetTasks.cs

--- NetTasks.cs
+++ NetTasks.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PremierLeagueDashboardApp
 {
@@ -18,20 +19,29 @@ namespace PremierLeagueDashboardApp
         private static string token = "-";
         public async static Task<Table> GetTable()
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("method", "GET");
-            client.DefaultRequestHeaders.Add("header", "X-Auth-Token: "+token);
-            
-            HttpResponseMessage response = await client.GetAsync(adress);
-            HttpContent content = response.Content;
-            string result = await content.ReadAsStringAsync();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("method", "GET");
+                client.DefaultRequestHeaders.Add("header", "X-Auth-Token: " + token);
 
-            if (result != null)
-            {
-                return Parser.ParseTable(result);
+                HttpResponseMessage response = await client.GetAsync(adress);
+                HttpContent content = response.Content;
+                string result = await content.ReadAsStringAsync();
+
+                if (result != null)
+                {
+                    return Parser.ParseTable(result);
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch
             {
+                MessageBox.Show("Loading table failed. Try later.",
+                    "Connection failed");
                 return null;
             }
         }
@@ -59,59 +69,86 @@ namespace PremierLeagueDashboardApp
 
         public async static Task<Team> GetTeam()
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("method", "GET");
-            client.DefaultRequestHeaders.Add("header", "X-Auth-Token: " + token);
-            HttpResponseMessage response = await client.GetAsync(team);
-            HttpContent content = response.Content;
-            string result = await content.ReadAsStringAsync();
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("method", "GET");
+                client.DefaultRequestHeaders.Add("header", "X-Auth-Token: " + token);
+                HttpResponseMessage response = await client.GetAsync(team);
+                HttpContent content = response.Content;
+                string result = await content.ReadAsStringAsync();
 
-            if (result != null)
-            {
-                return Parser.ParseTeam(result);
+                if (result != null)
+                {
+                    return Parser.ParseTeam(result);
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch
             {
+                MessageBox.Show("Loading team failed. Try later.",
+                    "Connection failed");
                 return null;
             }
         }
 
         public async static Task<TeamPlayers> GetPlayers()
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("method", "GET");
-            client.DefaultRequestHeaders.Add("header", "X-Auth-Token: " + token);
-
-            HttpResponseMessage response = await client.GetAsync(players);
-            HttpContent content = response.Content;
-            string result = await content.ReadAsStringAsync();
-
-            if (result != null)
+            try
             {
-                return Parser.ParsePlayers(result);
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("method", "GET");
+                client.DefaultRequestHeaders.Add("header", "X-Auth-Token: " + token);
+
+                HttpResponseMessage response = await client.GetAsync(players);
+                HttpContent content = response.Content;
+                string result = await content.ReadAsStringAsync();
+
+                if (result != null)
+                {
+                    return Parser.ParsePlayers(result);
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch
             {
+                MessageBox.Show("Loading team players failed. Try later.",
+                    "Connection failed");
                 return null;
             }
         }
 
         public async static Task<AllFixtures> GetAllFixtures()
         {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Add("method", "GET");
-            client.DefaultRequestHeaders.Add("header", "X-Auth-Token: " + token);
-
-            HttpResponseMessage response = await client.GetAsync(allFixtures);
-            HttpContent content = response.Content;
-            string result = await content.ReadAsStringAsync();
-
-            if(result != null)
+            try
             {
-                return Parser.ParseAllFixtures(result);
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Add("method", "GET");
+                client.DefaultRequestHeaders.Add("header", "X-Auth-Token: " + token);
+
+                HttpResponseMessage response = await client.GetAsync(allFixtures);
+                HttpContent content = response.Content;
+                string result = await content.ReadAsStringAsync();
+
+                if (result != null)
+                {
+                    return Parser.ParseAllFixtures(result);
+                }
+                else
+                {
+                    return null;
+                }
             }
-            else
+            catch
             {
+                MessageBox.Show("Loading fixtures failed. Try later.",
+                    "Connection failed");
                 return null;
             }
         }
